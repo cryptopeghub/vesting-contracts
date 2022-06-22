97d2c0cb3be3e4dedd1e5093b97cc08d21380ff7
CryptoAaron <aaron@1coin.io>
2022-06-22 17:49:54 -0400

    small change

--- deploy/Vesting.js
+++ deploy/Vesting.js
@@ -5,13 +5,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     const trezor = '0x49707808908f0C2450B3F2672E012eDBf49eD808';
 
-    // const emp = await deploy('CZempToken', {
-    //     from: deployer,
-    //     args: ['CZpegs EMP', 'CZemp', 18, dev],
-    //     log: true,
-    //     deterministicDeployment: false,
-    // });
-    // console.log('EMP', emp.address);
+
     const armory = await deploy('PHUBVesting', {
         from: deployer,
 
